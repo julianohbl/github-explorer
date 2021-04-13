918d542008d047ecc842776e31d81430d91db755
Juliano <julianohbl@hotmail.com>
2021-04-13 15:17:35 -0300

    Styling Repository

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +11,55 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/15468735?v=4"
+            alt="Juliano"
+          />
+          <div>
+            <strong>julianohbl/pagina-inicial-instagram-d-i-o</strong>
+            <p>Descrição deo repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>15</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>2</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>7</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="aeuhfsiuf">
+          <div>
+            <strong>aweshsfiuh</strong>
+            <p>asweiufhsiufgh</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
